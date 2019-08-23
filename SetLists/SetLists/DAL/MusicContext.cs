3f823cefad6918bf36b1b5df9c8dd9a1ae750d67
Your Name <you@example.com>
2019-08-23 14:44:42 -0700

    Initial commit

---
+++ SetLists/SetLists/DAL/MusicContext.cs
@@ -0,0 +1,37 @@
+﻿using SetLists.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace SetLists.DAL
+{
+    public class MusicContext : DbContext
+    {
+        public MusicContext() : base("MusicContext")
+        {
+        }
+
+        public DbSet<Song> Songs { get; set; }
+        public DbSet<SetList> SetLists { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Entity<SetList>()
+                .HasMany(t => t.Songs)
+                .WithMany(t => t.SetLists)
+                .Map(mc =>
+                {
+                    mc.ToTable("SetListSongs");
+                    mc.MapLeftKey("SetListID");
+                    mc.MapRightKey("SongID");
+                }
+                );
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ SetLists/SetLists/Models/SetList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SetLists.Models
+{
+    public class SetList
+    {
+        //public SetList()
+        //{
+        //    Songs = new List<Song>();
+        //}
+        public int SetListID { get; set; }
+        public string Title { get; set; }
+
+        public virtual ICollection<Song> Songs { get; set; }
+    }
+}
